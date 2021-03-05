ab093b97034d6c2af803b0c6d90b718d60809c6c
nyancermc <nyancer50213@gmail.com>
2021-03-05 19:12:43 +0900

    add tofufence and tofu ladder

--- src/main/java/baguchan/tofucraft/registry/TofuBlocks.java
+++ src/main/java/baguchan/tofucraft/registry/TofuBlocks.java
@@ -87,6 +87,21 @@ public class TofuBlocks {
 	public static final Block WALLTOFUTORCH_METAL = new WallTorchBlock(Block.Properties.create(Material.MISCELLANEOUS).hardnessAndResistance(0.0F, 7.5F).setLightLevel((state) -> {
 		return 14;
 	}).doesNotBlockMovement().notSolid().sound(SoundType.METAL).lootFrom(TOFUTORCH_METAL), ParticleTypes.FLAME);
+
+	//Ladder
+	public static final Block TOFULADDER_KINU = new LadderBlock(Block.Properties.from(KINUTOFU));
+	public static final Block TOFULADDER_MOMEN = new LadderBlock(Block.Properties.from(MOMENTOFU));
+	public static final Block TOFULADDER_ISHI = new LadderBlock(Block.Properties.from(ISHITOFU));
+	public static final Block TOFULADDER_METAL = new LadderBlock(Block.Properties.from(METALTOFU));
+	//public static final Block TOFULADDER_ZUNDABRICK = new TofuLadderBlock(Block.Properties.from(ZUNDATOFU_BRICK));
+	public static final Block TOFULADDER_ISHIBRICK = new LadderBlock(Block.Properties.from(ISHITOFU_BRICK));
+	//FENCE
+	public static final Block TOFUFENCE_KINU = new WallBlock(Block.Properties.create(TofuMaterial.TOFU).harvestTool(ToolType.SHOVEL).hardnessAndResistance(0.1F, 0.2F).sound(SoundType.SNOW));
+	public static final Block TOFUFENCE_MOMEN = new WallBlock(Block.Properties.create(TofuMaterial.TOFU).harvestTool(ToolType.SHOVEL).hardnessAndResistance(0.35F, 0.5F).sound(SoundType.SNOW));
+	public static final Block TOFUFENCE_ISHI = new WallBlock(Block.Properties.create(Material.ROCK).harvestTool(ToolType.PICKAXE).hardnessAndResistance(1.5F, 6.0F).sound(SoundType.STONE));
+	public static final Block TOFUFENCE_METAL = new WallBlock(Block.Properties.from(METALTOFU));
+
+
 	//terrain
 	public static final Block TOFU_TERRAIN = new Block(AbstractBlock.Properties.create(TofuMaterial.TOFU).harvestTool(ToolType.SHOVEL).hardnessAndResistance(0.5F, 1.0F).sound(SoundType.SNOW));
 	//misc
@@ -154,6 +169,17 @@ public class TofuBlocks {
 		registry.getRegistry().register(WALLTOFUTORCH_MOMEN.setRegistryName("walltofutorch_momen"));
 		registry.getRegistry().register(WALLTOFUTORCH_ISHI.setRegistryName("walltofutorch_ishi"));
 		registry.getRegistry().register(WALLTOFUTORCH_METAL.setRegistryName("walltofutorch_metal"));
+		//Ladder
+		registry.getRegistry().register(TOFULADDER_KINU.setRegistryName("tofuladder_kinu"));
+		registry.getRegistry().register(TOFULADDER_MOMEN.setRegistryName("tofuladder_momen"));
+		registry.getRegistry().register(TOFULADDER_ISHI.setRegistryName("tofuladder_ishi"));
+		registry.getRegistry().register(TOFULADDER_ISHIBRICK.setRegistryName("tofuladder_ishibrick"));
+		registry.getRegistry().register(TOFULADDER_METAL.setRegistryName("tofuladder_metal"));
+		//FENCE
+		registry.getRegistry().register(TOFUFENCE_KINU.setRegistryName("tofufence_kinu"));
+		registry.getRegistry().register(TOFUFENCE_MOMEN.setRegistryName("tofufence_momen"));
+		registry.getRegistry().register(TOFUFENCE_ISHI.setRegistryName("tofufence_ishi"));
+		registry.getRegistry().register(TOFUFENCE_METAL.setRegistryName("tofufence_metal"));
 
 		registry.getRegistry().register(TOFU_TERRAIN.setRegistryName("tofu_terrain"));
 
